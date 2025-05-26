2d709d31b6543ff193c2e9f4d4c22842597a97a4
vesrntechnologies <vennjoy@gmail.com>
2025-05-26 13:07:39 +0530

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -106,6 +106,140 @@ let questions = [
       "29"
     ]
   },
+      {
+    numb: 10,
+    question: "What is the formula for the Quantity of electricity (Q)?",
+ 
+    answer: "Current x Time",
+    options: [
+      "Current x Time",
+      "Voltage x Current",
+      "Current x Resistance",
+      "Voltage x Resistance"
+    ]
+  },
+     {
+    numb: 11,
+    question: "Which formula is used to calculate the power of a DC circuit?",
+  
+    answer: "Current x voltage",
+    options: [
+      "Voltage x time",
+      "Current x voltage",
+      "Current x resistance",
+      "Voltage x resistance"
+    ]
+  },
+     {
+    numb: 12,
+    question: "What is the advantage of the stranded conductor over the solid conductor?",
+    
+    answer: "More flexible",
+    options: [
+      "cost is less",
+      "More flexible",
+      "less voltage drop",
+      "more insulation resistance"
+    ]
+  },
+ {
+    numb: 13,
+    question: "What is the advantage of the crimping terminations?",
+ 
+    answer: "Avoid loose connections",
+    options: [
+      "Increase the voltage",
+      "Reduce the load current",
+      "Avoid loose connections",
+      "Easy to replace"
+    ]
+  },
+{
+    numb: 14,
+    question: "Which part of the underground cable is protecting the metallic sheath against corrosion?",
+    
+    answer: "Bedding",
+  
+    options: [
+      "Serving",
+      "Bedding",
+      "Armouring",
+      "Lead Sheath"
+    ]
+  },
+    {
+    numb: 15,
+    question: "What is the use of dipsoldering method?",
+  
+    answer: "Soldering miniature components on PCB",
+    options: [
+      "Hard soldering",
+      "Piping and cable soldering work",
+      "Soldering miniature components on PCB",
+     "soldering of tin sheets"
+    ]
+  },
+    {
+    numb: 16,
+    question: "Which is the semiconductor material?",
+    
+    answer: "Germanium",
+    options: [
+      "Eureka",
+      "Ebonite",
+      "Maganin",
+      "Germanium"
+    ]
+  },
+    {
+    numb: 17,
+    question: "What is the reading of the micrometer as shown in the figure?",
+      immg:"img17",
+    answer: "4.55mm",
+    options: [
+      "5.05mm",
+      "5.00mm",
+      "4.55mm",
+      "4.05mm"
+    ]
+  },
+     {
+    numb: 18,
+    question: "Which is used to measure the size of the conductor?",
+   
+    answer:"Standard wire gauge",
+    options: [
+      "Bevel gauge",
+      "Vernier caliper",
+      "Standard wire gauge",
+      "Depth gauge"
+    ]
+  },
+       {
+    numb: 19,
+    question: "What is the purpose of serving layer in underground cable?",
+    
+    answer:"Protect armouring from atmospheric condition",
+    options: [
+      "Protect the cable from mechanical vibration",
+      "Protect the cable from mechanical injury",
+      "Protect metallic sheath against corrosion",
+      "Protect armouring from atmospheric condition"
+    ]
+  },
+        {
+    numb: 20,
+    question: "Which type of joint is used for extending the length of conductor in over head lines?",
+   
+    answer:"Western Union joint",
+    options: [
+      "Scarfed joint",
+      "Aerial tap joint",
+      "Britannia T joint",
+      "Western Union joint"
+    ]
+  },  
+
 
   // you can uncomment the below codes and make duplicate as more as you want to add question
   // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
