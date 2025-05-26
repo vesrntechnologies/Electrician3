f0a38a15b6be421c19b43bc1e765f180e503ca98
vesrntechnologies <vennjoy@gmail.com>
2025-05-26 13:04:29 +0530

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -12,6 +12,100 @@ let questions = [
       "Insulation Resistance Test"
     ]
   },
+     {
+    numb: 2,
+    question: "What does the number 1.40 represent if a stranded conductor is designated as 7/1.40?",
+    answer: "Diameter of each conductor",
+    options: [
+      "Area of cross section",
+      "Radius of one conductor",
+      "Diameter of all conductor", 
+      "Diameter of each conductor"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the size of the neutral core of a 90 sq mm 31/2 core U.G cable?",
+    
+    answer: "50 Sq mm",
+    options: [
+      "45 Sq mm",
+      "50 Sq mm",
+      "70 Sq mm",
+      "90 Sq mm"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the name of the tool as shown in the figure?",
+    immg:"img4",
+    answer: "Crimping tool",
+    options: [
+      "cutting plier",
+      "Wire stripper",
+      "Crimping tool",
+      "Side cutting plier"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which type of joint is used in over head lines for high tensile strength?",
+    answer: "Western union joint",
+    options: [
+      "Scarfed joint",
+      "Married Joint",
+      "Western union joint",
+      "Rat tail joint"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What is the unit for quantity of electricity?",
+    answer: "Coulomb",
+    options: [
+      "Mho",
+      "Coulomb",
+      "Volt/second",
+      "Ampere/second"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What is the name of the soldering method as shown in the figure? ",
+    immg:"img7",
+    answer: "Soldering gun",
+    options: [
+      "Dip Soldering",
+      "Soldering iron",
+      "Soldering gun",
+      "soldering with flame"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What is the name of the soldering method as shown in the figure?",
+    immg:"img8",
+    answer: "Temperature controlled soldering",
+    options: [
+      "Dip Soldering",
+      "Soldering with blow lamp",
+      "Soldering with soldering gun",
+      "Temperature controlled soldering"
+    ]
+  },
+      {
+    numb: 9,
+    question: "How many electrons are there in the third shell of the copper atom?",
+    
+    answer: "18",
+ 
+    options: [
+      "8",
+      "13",
+      "18",
+      "29"
+    ]
+  },
 
   // you can uncomment the below codes and make duplicate as more as you want to add question
   // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
